67a13666c2ab5637afe9241938192fb03da762eb
Theo Justman <justmant@mylanecc.edu>
2025-05-18 20:54:41 -0700

    Eliza comments

--- midterm_master/Midterm_Core5/Eliza/Program.cs
+++ midterm_master/Midterm_Core5/Eliza/Program.cs
@@ -6,22 +6,44 @@ namespace Eliza
     {
         static string CreateElizaResponse(string clientStatement)
         {
+            //Remove capitilization
             string statement = clientStatement.ToLower();
+            //Check for key words
             if (statement.Contains("my"))
             {
-
-            } else if statement.Contains("love") || statement.Contains("hate") {
-                return "You have strong feelings about that!";
-            } else {
-                defaultResponces = { };
-                //int randomIndex = Random
-                return defaultResponses[randomIndex];
+                //Get word after my
+                int myIndex = statement.IndexOf("my ");
+                string afterMy = statement.Substring(myIndex + 3);
+                string[] splitStatement = afterMy.Split(' ');
+                //Return to be displayed
+                return $"Tell me more about your {splitStatement[0]}.";
             }
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-            string response = CreateElizaResponse(input);
-            Console.WriteLine(response);
+            else if (statement.Contains("love") || statement.Contains("hate"))
+            {
+                //Consistent responce
+                return "You have strong feelings about that!";
+            }
+            else
+            {
+                //Randomized is no key words
+                string[] defaultResponces = { "Please go on.", "Tell me more", "Continue." };
+                Random random = new Random();
+                int randomIndex = random.Next(2);
+                return defaultResponces[randomIndex];
+            }
         }
-    }
+            static void Main(string[] args)
+            {
+                //Introduction
+                Console.WriteLine("Hello! I am Eliza, here for all of your mental health needs. What is on your mind?");
+                //Loop forever
+                do
+                {
+                    string input = Console.ReadLine();
+                    string response = CreateElizaResponse(input);
+                    Console.WriteLine(response);
+                }
+                while (true);
+            }
+        }
 }
