fb925483ae40ae35317a21cd3d5e15f26d72186a
Theo Justman <justmant@mylanecc.edu>
2025-05-18 21:35:36 -0700

    Palindrome comments

--- midterm_master/Midterm_Core5/Palindrome/Program.cs
+++ midterm_master/Midterm_Core5/Palindrome/Program.cs
@@ -7,15 +7,19 @@ namespace Palindrome
     {
         static bool IsPalindrome(string text)
         {
+            //Set up stacks
             Stack<char> stack = new Stack<char>();
             Queue<char> queue = new Queue<char>();
 
+            //Put input into stack and queue in desired order
             foreach (char c in text){
                 stack.Push(c);
                 queue.Enqueue(c);
             }
+            //Go until all checked
             while (stack.Count > 0 && queue.Count > 0)
             {
+                //Checks "adjacent" characters
                 if (stack.Pop() != queue.Dequeue())
                 {
                     return false;
@@ -26,14 +30,17 @@ namespace Palindrome
         }
         static void Main(string[] args)
         {
+            //Introduction
             Console.WriteLine("Please enter a word or phrase to be checked as a Palindrome.");
             string input = Console.ReadLine();
+            //Remove punctuation and capitilization
             string cleanedInput = "";
             foreach (char c in input) {
                 if (char.IsLetterOrDigit(c)) {
                     cleanedInput += char.ToLower(c);
                 }
             }
+            //Shows result
             if (IsPalindrome(cleanedInput))
             {
                 Console.WriteLine($"{input} is a palindrome.");
