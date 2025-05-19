ba9c2d6f27f7e4cd6462505d98f2c062cfee0f25
Theo Justman <justmant@mylanecc.edu>
2025-05-18 20:23:30 -0700

    Eliza first draft #nonfunctinal

--- midterm_master/Midterm_Core5/Eliza/Program.cs
+++ midterm_master/Midterm_Core5/Eliza/Program.cs
@@ -4,9 +4,24 @@ namespace Eliza
 {
     class Program
     {
+        static string CreateElizaResponse(string clientStatement)
+        {
+            string statement = clientStatement.ToLower();
+            if (statement.Contains("my"))
+            {
+
+            } else if statement.Contains("love") || statement.Contains("hate") {
+                return "You have strong feelings about that!";
+            } else {
+                defaultResponces = { };
+                //int randomIndex = Random
+                return defaultResponses[randomIndex];
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string response = CreateElizaResponse(input);
+            Console.WriteLine(response);
         }
     }
 }
