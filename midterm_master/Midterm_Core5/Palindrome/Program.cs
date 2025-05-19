a40f1176f508837aa787c99fdc42e862160ed98e
Theo Justman <justmant@mylanecc.edu>
2025-05-18 21:22:09 -0700

    Palindrome draft

--- midterm_master/Midterm_Core5/Palindrome/Program.cs
+++ midterm_master/Midterm_Core5/Palindrome/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Palindrome
 {
     class Program
     {
+        static bool IsPalindrome(string text)
+        {
+            Stack<char> stack = new Stack<char>();
+            Queue<char> queue = new Queue<char>();
+
+            foreach (char c in text){
+                stack.Push(c);
+                queue.Enqueue(c);
+            }
+            while (stack.Count > 0 && queue.Count > 0)
+            {
+                if (stack.Pop() != queue.Dequeue())
+                {
+                    return false;
+                }
+                return true;
+            
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a word or phrase to be checked as a Palindrome.");
+            string input = Console.ReadLine();
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine($"{input} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not a palindrome.");
+            }
         }
     }
 }
