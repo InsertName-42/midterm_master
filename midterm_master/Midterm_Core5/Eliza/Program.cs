67fad63f03b01242c17d25879740ba652d62e6fd
InsertName-42 <justmant@my.lanecc.edu>
2026-05-18 21:03:18 -0700

    Regex and typos

--- midterm_master/Midterm_Core5/Eliza/Program.cs
+++ midterm_master/Midterm_Core5/Eliza/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Eliza
 {
     class Program
     {
+        private static readonly Random _random = new Random();
         static string CreateElizaResponse(string clientStatement)
         {
             //Remove capitilization
             string statement = clientStatement.ToLower();
             //Check for key words
-            if (statement.Contains("my"))
+            if (Regex.IsMatch(statement, @"\bmy\b"))
             {
                 //Get word after my
-                int myIndex = statement.IndexOf("my ");
+                Match match = Regex.Match(statement, @"\bmy\b");
+                int myIndex = match.Index;
+
                 string afterMy = statement.Substring(myIndex + 3);
                 string[] splitStatement = afterMy.Split(' ');
                 //Return to be displayed
@@ -20,16 +24,15 @@ namespace Eliza
             }
             else if (statement.Contains("love") || statement.Contains("hate"))
             {
-                //Consistent responce
+                //Consistent response
                 return "You have strong feelings about that!";
             }
             else
             {
                 //Randomized is no key words
-                string[] defaultResponces = { "Please go on.", "Tell me more", "Continue." };
-                Random random = new Random();
-                int randomIndex = random.Next(2);
-                return defaultResponces[randomIndex];
+                string[] defaultResponses = { "Please go on.", "Tell me more", "Continue." };
+                int randomIndex = _random.Next(defaultResponses.Length);
+                return defaultResponses[randomIndex];
             }
         }
             static void Main(string[] args)
